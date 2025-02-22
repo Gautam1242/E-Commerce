d3f8f8081e8d2b981a3224224273c2f4bb5e16c4
Gautam Shah <gautamshah317@gmail.com>
2025-02-22 22:27:40 +0530

    working on admin orders page

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -47,7 +47,19 @@ const placeOrderRazorpay=async(req,res)=>{
 
 // All orders data for admin panel
 const allOrders=async(req,res)=>{
-
+    try {
+        const orders=await orderModel.find({});
+        res.json({
+            success:true,
+            orders
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success:false,
+            message:error.message
+        })
+    }
 }
 
 // User order data for frontend for individual user
