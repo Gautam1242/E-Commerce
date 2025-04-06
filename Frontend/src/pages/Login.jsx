74bce7d90cf133bd0de649768c387c83f0421df1
Gautam Shah <gautamshah317@gmail.com>
2025-04-06 09:49:20 +0530

    removed the unusable LOC

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -25,7 +25,6 @@ const Login = () => {
         if (response.data.success) {
           setToken(response.data.token);
           localStorage.setItem("token", response.data.token);
-          // toast.success(response.data.message);
           toast.success("Signup Successfully");
         } else {
           toast.error(response.data.message);
