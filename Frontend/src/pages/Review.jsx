c12c7b0072dab6e82b0364c81e01788f9918fc08
Gautam Shah <gautamshah317@gmail.com>
2025-04-26 12:43:48 +0530

    final code review and debugging

---
+++ Frontend/src/pages/Review.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "../components/Title";
+const Review = () => {
+  return (
+    <div className="text-center text-2xl pt-10 border-t">
+      <Title text1={"Product"} text2={"Reviews"} />
+    </div>
+  );
+};
+
+export default Review;
